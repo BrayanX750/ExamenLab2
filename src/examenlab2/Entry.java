dc7cd8216a31137bfbb7365dfc4019e5aa9f9439
BrayanX750 <brayan_pinel@unitec.edu>
2025-03-28 14:02:21 -0600

    Creacion clase entry

---
+++ src/examenlab2/Entry.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlab2;
+
+public class Entry {
+    String username;
+    long pos;
+    Entry next;
+
+    public Entry(String username, long pos) {
+        this.username = username;
+        this.pos = pos;
+        this.next = null;
+    }
+}
